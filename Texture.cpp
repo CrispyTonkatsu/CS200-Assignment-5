dd692be8d53bec8ff460c902c29263a661d979e0
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2024-10-29 23:47:10 -0700

    Fixed rounding issue

--- Texture.cpp
+++ Texture.cpp
@@ -8,8 +8,6 @@
 
 #include "Texture.h"
 #include <cmath>
-#include <iostream>
-#include <ostream>
 #include <tuple>
 #include "Affine.h"
 
@@ -21,15 +19,15 @@ calculateColor(glm::mat4 &transform, size_t x, size_t y) {
   float sum = (texture_coordinates.x - 0.5f) * (texture_coordinates.x - 0.5f) +
               (texture_coordinates.y - 0.5f) * (texture_coordinates.y - 0.5f);
 
-  unsigned char r = 100 + (150 * texture_coordinates.x);
-  unsigned char b = 100 + (150 * texture_coordinates.y);
+  unsigned char r = 100 + (155 * texture_coordinates.x);
+  unsigned char b = 100 + (155 * texture_coordinates.y);
 
   if (sum > 0.16f) {
     return std::make_tuple(r, 0, b);
   }
 
   if (sum < 0.09f) {
-    return std::make_tuple(r, 0, b);
+    return std::make_tuple(b, 0, r);
   }
 
   return std::make_tuple(0, 255, 0);
@@ -40,10 +38,10 @@ cs200::Bitmap::Bitmap(unsigned W, unsigned H) :
 
   glm::mat4 bitmap_to_texture = cs200::bitmapToTextureTransform(*this);
 
-  for (size_t i = 0; i < bmp_height; i++) {
-    for (size_t j = 0; j < bmp_width; j++) {
+  for (size_t j = 0; j < bmp_height; j++) {
+    for (size_t i = 0; i < bmp_width; i++) {
       std::tuple<unsigned char, unsigned char, unsigned char> rgb =
-          calculateColor(bitmap_to_texture, j, i);
+          calculateColor(bitmap_to_texture, i, j);
 
       bmp_data.push_back(std::get<0>(rgb));
       bmp_data.push_back(std::get<1>(rgb));
@@ -66,7 +64,9 @@ unsigned cs200::computeStride(unsigned W) {
 void cs200::reverseRGB(Bitmap &) {}
 
 glm::mat4 cs200::bitmapToTextureTransform(const Bitmap &b) {
-  return scale(1 / b.width(), 1 / b.height()) * // NOLINT *magic*
+  return scale(
+             1.f / static_cast<float>(b.width()),
+             static_cast<float>(1.f / b.height())) * // NOLINT *magic*
          translate(vector(0.5f, 0.5f)); // NOLINT *magic*
 }
 
@@ -86,13 +86,13 @@ float textureWrap(float x) {
 glm::vec3 cs200::getColor(const Bitmap &b, float u, float v) {
   // TODO: Add exception throw
 
-  // BUG: There seems to be an issue here with the texture coordinates
   float wrapped_u = textureWrap(u);
   float wrapped_v = textureWrap(v);
 
   glm::vec4 bitmap_coordinates =
-      textureToBitmapTransform(b) * vector(wrapped_u, wrapped_v);
+      textureToBitmapTransform(b) * point(wrapped_u, wrapped_v);
 
+  // TODO: add rounding
   unsigned offset = b.offset(bitmap_coordinates.x, bitmap_coordinates.y);
 
   return {b.data()[offset], b.data()[offset + 1], b.data()[offset + 2]};
